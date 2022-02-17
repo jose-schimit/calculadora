701461b3dc766428b3c989b0c10b1c6df7129fcd
jose-schimit <jose.schimit@hotmail.com>
2022-02-17 16:02:57 -0300

    v2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//função para limpar o display
 function limpar(){
     document.getElementById("visor").innerHTML = 0
 }
 
+//funcão para setar o némero zero no display
 function setaZero() {
     var value = document.getElementById("visor").innerHTML;
     if (value == "0") {
@@ -10,17 +12,21 @@ function setaZero() {
     }
 }
 
+//função para setar o valor digitado na calculadora
 function display(value) {
     setaZero()
     document.getElementById("visor").innerHTML += value;
 }
 
+//função para realizar a operação e setar o resultado no display
 function calcular() {
     setaZero()
     var equacao = document.getElementById("visor").innerHTML;
     var resultado = eval(equacao);
     document.getElementById('visor').innerHTML = resultado;
 }
+
+//função para calcular porcentagem e setar o valor no display
 function porcentagem() {
     var value = document.getElementById("visor").innerHTML;
     value = value / 100;
